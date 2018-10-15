669af831c8f1102f145bcd239bca4001d471da84
Elias Correa Silva <correa.elias@gmail.com>
2018-10-15 16:10:51 -0300

    Code improvement.
    -Moved 'FormatSettings' initialization to 'tkFloat' case.
    -Removed useless string concatenations.
    -Removed the need for the 'TypeData' variable.
    -Added ifdef to handle a name change in trunk (ManagedFldCount to TotalFieldCount).

--- TypeUtils.pas
+++ TypeUtils.pas
@@ -1,94 +1,115 @@
-unit TypeUtils;
-
 {$mode objfpc}
+
+{
+TODO:
+- Write my own VariantToString for consistency (for string and float point numbers)
+- Write multimensional arrays like a table???
+}
+
+unit TypeUtils;
 
 interface
 
 uses sysutils, typinfo, variants;
 
-function ToStr(var AX; ATypeInfo: PTypeInfo): AnsiString;
+function ToStr(const AValue; ATypeInfo: PTypeInfo): AnsiString;
 
 implementation
 
-function ToStr(var AX; ATypeInfo: PTypeInfo): AnsiString;
+function ToStr(const AValue; ATypeInfo: PTypeInfo): AnsiString;
 type
   TArray = array of Byte;
 var
   I: LongInt;
-  TypeData: PTypeData;
   FormatSettings: TFormatSettings;
+  FirstField, Field: PManagedField;
+  ElementSize: SizeInt;
 begin
-  FillByte(FormatSettings, SizeOf(TFormatSettings), 0);
-  FormatSettings.DecimalSeparator := '.';
-  TypeData := GetTypeData(ATypeInfo);
-  Result := '';
   case ATypeInfo^.Kind of
-    tkChar: Result += '''' + Char(AX) + '''';
-    tkWChar: Result += '''' + AnsiString(WChar(AX)) + '''';
-    tkBool: Result += BoolToStr(Boolean(AX), True);
+    tkChar: Result := QuotedStr(Char(AValue));
+    tkWChar: Result := QuotedStr(AnsiString(WChar(AValue)));
+    tkBool: Result := BoolToStr(Boolean(AValue), True);
+    tkInt64: Result := IntToStr(Int64(AValue));
+    tkQWord: Result := IntToStr(QWord(AValue));
+    tkSString: Result := QuotedStr(ShortString(AValue));
+    tkAString: Result := QuotedStr(AnsiString(AValue));
+    tkWString: Result := QuotedStr(AnsiString(WideString(AValue)));
+    tkUString: Result := QuotedStr(AnsiString(UnicodeString(AValue)));
+    tkClass: Result := TObject(AValue).ToString;
+    tkEnumeration: Result := GetEnumName(ATypeInfo, Integer(AValue));
+    tkSet: Result := SetToString(ATypeInfo, Integer(AValue), True).Replace(',', ', ');
+    tkVariant: Result := VarToStr(Variant(AValue));
     tkInteger:
     begin
-      case TypeData^.OrdType of
-        otSByte: Result += IntToStr(ShortInt(AX));
-        otUByte: Result += IntToStr(Byte(AX));
-        otSWord: Result += IntToStr(SmallInt(AX));
-        otUWord: Result += IntToStr(Word(AX));
-        otSLong: Result += IntToStr(LongInt(AX));
-        otULong: Result += IntToStr(LongWord(AX));
+      case GetTypeData(ATypeInfo)^.OrdType of
+        otSByte: Result := IntToStr(ShortInt(AValue));
+        otUByte: Result := IntToStr(Byte(AValue));
+        otSWord: Result := IntToStr(SmallInt(AValue));
+        otUWord: Result := IntToStr(Word(AValue));
+        otSLong: Result := IntToStr(LongInt(AValue));
+        otULong: Result := IntToStr(LongWord(AValue));
       end;
     end;
-    tkInt64: Result += IntToStr(Int64(AX));
-    tkQWord: Result += IntToStr(QWord(AX));
     tkFloat:
     begin
-      case TypeData^.FloatType of
-        ftSingle: Result += FormatFloat('0.###', Single(AX), FormatSettings);
-        ftDouble: Result += FormatFloat('0.###', Double(AX), FormatSettings);
-        ftExtended: Result += FormatFloat('0.###', Extended(AX), FormatSettings);
-        ftComp: Result += FormatFloat('0.###', Comp(AX), FormatSettings);
-        ftCurr: Result += FormatFloat('0.###', Currency(AX), FormatSettings);
+      FillByte(FormatSettings, SizeOf(TFormatSettings), 0);
+      FormatSettings.DecimalSeparator := '.';
+      case GetTypeData(ATypeInfo)^.FloatType of
+        ftSingle: Result := FormatFloat('0.######', Single(AValue), FormatSettings);
+        ftDouble: Result := FormatFloat('0.######', Double(AValue), FormatSettings);
+        ftExtended: Result := FormatFloat('0.######', Extended(AValue), FormatSettings);
+        ftComp: Result := FormatFloat('0.######', Comp(AValue), FormatSettings);
+        ftCurr: Result := FormatFloat('0.######', Currency(AValue), FormatSettings);
       end;
     end;
-    tkSString: Result += '''' + ShortString(AX) + '''';
-    tkAString: Result += '''' + AnsiString(AX) + '''';
-    tkWString: Result += '''' + AnsiString(WideString(AX)) + '''';
-    tkUString: Result += '''' + AnsiString(UnicodeString(AX)) + '''';
     tkRecord:
     begin
-      Result += '(';
-      for I := 0 to TypeData^.ManagedFldCount - 1 do
+      Result := '(';
+      with GetTypeData(ATypeInfo)^ do
       begin
-        if I > 0 then Result += ', ';
-        with PManagedField(PByte(@TypeData^.ManagedFldCount) + SizeOf(TypeData^.ManagedFldCount) + (I * SizeOf(TManagedField)))^ do
-          Result += ToStr((PByte(@AX) + FldOffset)^, TypeRef);
+        {$IFNDEF VER3_0} //ifdef needed because of a field rename in trunk (ManagedFldCount to TotalFieldCount)
+        FirstField := PManagedField(PByte(@TotalFieldCount) + SizeOf(TotalFieldCount));
+        for I := 0 to TotalFieldCount - 1 do
+        {$ELSE}
+        FirstField := PManagedField(PByte(@ManagedFldCount) + SizeOf(ManagedFldCount));
+        for I := 0 to ManagedFldCount - 1 do
+        {$ENDIF}
+        begin
+          if I > 0 then Result += ', ';
+          Field := PManagedField(PByte(FirstField) + (I * SizeOf(TManagedField)));
+          Result += ToStr((PByte(@AValue) + Field^.FldOffset)^, Field^.TypeRef);
+        end;
       end;
       Result += ')';
     end;
     tkArray:
     begin
-      Result += '[';
-      for I := 0 to TypeData^.ArrayData.ElCount - 1 do
+      Result := '[';
+      with GetTypeData(ATypeInfo)^ do
       begin
-        if I > 0 then Result += ', ';
-        Result += ToStr((PByte(@AX) + (I * (TypeData^.ArrayData.Size div TypeData^.ArrayData.ElCount)))^, TypeData^.ArrayData.ElType);
+        ElementSize := ArrayData.Size div ArrayData.ElCount;
+        for I := 0 to ArrayData.ElCount - 1 do
+        begin
+          if I > 0 then Result += ', ';
+          Result += ToStr((PByte(@AValue) + (I * ElementSize))^, ArrayData.ElType);
+        end;
       end;
       Result += ']';
     end;
     tkDynArray:
     begin
-      Result += '[';
-      for I := 0 to Length(TArray(AX)) - 1 do
+      Result := '[';
+      with GetTypeData(ATypeInfo)^ do
       begin
-        if I > 0 then Result += ', ';
-        Result += ToStr((PByte(@TArray(AX)[0]) + (I * TypeData^.ElSize))^, TypeData^.ElType2);
+        for I := 0 to Length(TArray(AValue)) - 1 do
+        begin
+          if I > 0 then Result += ', ';
+          Result += ToStr((PByte(@TArray(AValue)[0]) + (I * ElSize))^, ElType2);
+        end;
       end;
       Result += ']';
     end;
-    tkClass: Result += TObject(AX).ToString;
-    tkEnumeration: Result += GetEnumName(ATypeInfo, Integer(AX));
-    tkSet: Result += SetToString(ATypeInfo, Integer(AX), True).Replace(',', ', ');
-    tkVariant: Result += VarToStr(Variant(AX)); //TODO: Write VariantToString for consistency (for string and float point numbers)
-    else Result += ATypeInfo^.Name + '@' + HexStr(@AX);
+    else Result := Format('%s@%p', [ATypeInfo^.Name, @AValue]);
   end;
 end;
 
